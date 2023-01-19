635b3ced6ae4b41df728cdc7962e1297f8b60b75
Nandlal <shahnandlal49@gmail.com>
2023-01-19 16:24:30 +0530

    added

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors=require("cors")
+const bodyparser = require("body-parser");
 const PORT = process.env.PORT;
 const UserRouter=require("./src/routes/user.route")
 const connect=require("./src/config/db")
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyparser.json());
 app.use(cors());
+mongoose.set("strictQuery", false);
 app.use("/user",UserRouter)
 app.get("/", (req, res) => res.send("hello"));
 
